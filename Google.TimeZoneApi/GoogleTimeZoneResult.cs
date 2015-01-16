a4c8d7ece9798290363d1e0e2d88cf8a06ba9ce4
Jaliya Udagedara <jaliya.udagedara@gmail.com>
2015-01-16 09:57:22 +0530

    Added field comments to GoogleTimeZoneResult.

--- Google.TimeZoneApi/GoogleTimeZoneResult.cs
+++ Google.TimeZoneApi/GoogleTimeZoneResult.cs
@@ -4,8 +4,28 @@ namespace Google.TimeZoneApi
 {
     public class GoogleTimeZoneResult
     {
+        /// <summary>
+        /// Gets or sets the date time.
+        /// </summary>
+        /// <value>
+        /// The date time.
+        /// </value>
         public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time zone identifier.
+        /// </summary>
+        /// <value>
+        /// The time zone identifier.
+        /// </value>
         public string TimeZoneId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the time zone.
+        /// </summary>
+        /// <value>
+        /// The name of the time zone.
+        /// </value>
         public string TimeZoneName { get; set; }
     }
 }
